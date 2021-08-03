ad4f988872ca72dbd1bd3a78178be0cf97078c49
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-04 02:09:37 +0600

    recursion file add

---
+++ factorial-recursion.js
@@ -0,0 +1,35 @@
+/*
+1! = 1
+2! = 2 X 1
+3! = 3 X 2 X 1
+4! = 4 X 3 X 2 X 1
+5! = 5 X 4 X 3 X 2 X 1
+6! = 6 X 5 X 4 X 3 X 2 X 1
+7! = 7 X 6 X 5 X 4 X 3 X 2 X 1
+n! = n * (n-1)!
+*/
+
+// onnothay
+
+/*
+1! = 1
+2! = 2 X 1!
+3! = 3 X 2!
+4! = 4 X 3!
+5! = 5 X 4!
+6! = 6 X 5!
+7! = 7 X 6!
+n! = n * (n-1)!
+*/
+
+const factorial = (n) => {
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+};
+
+const factNumber = 2;
+const facts = factorial(factNumber);
+console.log(facts);
