5dcb1d764a469944820b23750a3c59fc7e8e056c
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-04 01:55:41 +0600

    solve first commit

---
+++ factorial.js
@@ -0,0 +1,124 @@
+// factorial used for while loop 
+// let fact = 1;
+// let i = 1;
+// while(i <= 9) {
+//     fact = fact * i;
+//     i++
+// };
+
+// console.log(fact);
+
+// factorial used for for loop 
+// let fact = 1;
+// for (let i = 1; i <= 10; i++)
+// {
+//     fact = fact * i;
+// }
+
+// console.log(fact);
+
+// factorial used for for loop and function  
+
+// const factorial = (number) => {
+//     var fact = 1;
+//     for (var i = 1; i <= number; i++)
+//     {
+//         fact = fact * i;
+//     }
+//     return fact;
+// };
+
+// const facts = factorial(15);
+// console.log(facts);
+
+
+// const factorial = (number) => {
+//     let fact = 1;
+//     let i = 1;
+//     while (i <= number)
+//     {
+//         fact = fact * i;
+//         i++
+//     }
+//     return fact;
+// }
+
+// const fact_number = 68;
+// const facts = factorial(fact_number);
+// console.log(facts);
+
+
+// used to do loop 
+
+// const factorial = (number) => {
+//     let fact = 1;
+//     let i = 1;
+
+//     do
+//     {
+//         fact = fact * i;
+//         i++;
+//     } while (i <= number);
+
+//     return fact;
+// };
+
+// const factNumber = 68;
+// const facts = factorial(factNumber);
+// console.log(facts);
+
+
+
+// factorial boro theke suto 
+
+// const factorial = (number) => {
+//     let fact = 1;
+//     let i = number;
+
+//     while (i >= 1)
+//     {
+//         fact = fact * i;
+//         i--
+//     }
+//     return fact;
+// };
+
+// const facts = factorial(10);
+// console.log(facts);
+
+// again used to for loop
+
+// const factorial = (number) => {
+//     let fact = 1;
+//     for (let i = number; i >= 1; i--)
+//     {
+//         fact = fact * i;
+//     }
+//     return fact
+// };
+
+// const factNumber = 40;
+// const facts = factorial(factNumber);
+// console.log(facts);
+
+
+// used to do loop
+
+// const factorial = (number) => {
+//     let fact = 1;
+//     let i = number;
+
+//     do
+//     {
+//         fact = fact * i;
+//         i--
+//     } while (i >= 1);
+
+//     return fact;
+// }
+
+// const facts = factorial(40);
+// console.log(facts);
+
+
+////all about used to be : function , for loop , while loop , do loop //
