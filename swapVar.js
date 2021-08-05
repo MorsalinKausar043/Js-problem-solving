395b7a9dc06cbf4b419d1110807feeacc347fb5f
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-05 22:37:43 +0600

    add swapvar

---
+++ swapVar.js
@@ -0,0 +1,19 @@
+// only js
+
+// let first = 5;
+// let second = 7;
+// let temp = first;
+
+// first = second;
+// second = temp;
+// console.log(first , second);
+
+
+// es 6 
+
+let first = 5;
+let second = 7;
+
+[first, second] = [second, first];
+
+console.log(first , second);
