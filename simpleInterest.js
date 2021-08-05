4dbb96214c865853adedf2eda6039394743a913b
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-05 20:43:42 +0600

    interest for days,months,years

--- simpleInterest.js
+++ simpleInterest.js
@@ -1,11 +1,65 @@
-const simpleInterest = (initialAmount, interest, time) => {
-    
-    const A = initialAmount * (1 + interest / 100 * time);
-    return A;
-};
+// calculate for years
 
-const initialAmount = 300;
-const interest = 10;
-const time = 3;
-const simpleInteFun = simpleInterest(initialAmount, interest, time);
-console.log("Your Balance Is" , simpleInteFun);
+// const simpleInterest = (initialAmount, interest, time) => {
+    
+//     const A = initialAmount * (1 + interest / 100 * time);
+//     return A;
+// };
+
+// const initialAmount = 300;
+// const interest = 10;
+// const time = 3;
+// const simpleInteFun = simpleInterest(initialAmount, interest, time);
+// console.log("Your Balance Is" , simpleInteFun);
+
+// again calculate for months
+
+// const simpleInterest = (Amount, interest, time) => {
+    
+//     const times = time / 12;
+//     const Total = Amount * (1 + interest / 100 * times);
+
+//     return Total;
+// }
+
+// const initialAmount = 300;
+// const interest = 10;
+// const time = 23;
+// const simpleInteFun = simpleInterest(initialAmount, interest, time);
+// console.log("Your Balance Is" , simpleInteFun);
+
+
+// again calculate for days 365 days
+
+
+    // const simpleInterest = (Amount, interest, time) => {
+    
+    //     const times = time / 365;
+    //     const Total = Amount * (1 + interest / 100 * times);
+    
+    //     return Total;
+    // }
+    
+    // const initialAmount = 300;
+    // const interest = 10;
+    // const time = 23;
+    // const simpleInteFun = simpleInterest(initialAmount, interest, time);
+    // console.log("Your Balance Is" , simpleInteFun);
+
+
+// again calculate for days 360 days
+
+
+    // const simpleInterest = (Amount, interest, time) => {
+    
+    //     const times = time / 360;
+    //     const Total = Amount * (1 + interest / 100 * times);
+    
+    //     return Total;
+    // }
+    
+    // const initialAmount = 300;
+    // const interest = 10;
+    // const time = 23;
+    // const simpleInteFun = simpleInterest(initialAmount, interest, time);
+    // console.log("Your Balance Is" , simpleInteFun);
