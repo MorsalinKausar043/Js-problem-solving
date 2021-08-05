3c5f30d3d9d7f2c508e1eecd80807536c3adbe2c
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-05 22:51:18 +0600

    done

---
+++ max.js
@@ -0,0 +1,26 @@
+const x = 400;
+const y = 500;
+const z = 300;
+
+// const maxx = Math.max(x, y, z);
+// const maxxx = Math.min(x, y, z);
+
+// console.log(maxx);
+// console.log(maxxx);
+
+const findLargest = (x, y, z) => {
+    
+    const maxx = Math.max(x, y, z);
+    console.log(maxx);
+};
+
+findLargest(x, y, z)
+
+const findmini = (x, y, z) => {
+    
+    const maxxx = Math.min(x, y, z);
+    console.log(maxxx);
+};
+
+findmini(x,y,z)
+
