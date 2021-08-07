d3f6ef415119fdf6d1880438d67bd14b13f636e5
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-07 15:16:05 +0600

    done

--- sum.js
+++ sum.js
@@ -1,26 +1,8 @@
-let numbers = [2, 3, 5, 64, 6, 7,10];
+
 let sum = 0;
+for (let i = 0; i <= 3; i++)
+{
+    sum = sum + i;
+}
 
-// for (let i = 0; i < numbers.length; i++)
-// {
-//     const element = numbers[i];
-//     // sum = sum + element;
-//     sum += element;
-// };
-
-// console.log(sum);
-
-// used to function 
-
-const arryTotal = (numbers) => {
-    for (let i = 0; i < numbers.length; i++)
-    {
-        const element = numbers[i];
-        // sum = sum + element;
-        sum += element;
-    };
-
-    console.log(sum);
-};
-
-arryTotal(numbers)
+console.log(sum);
