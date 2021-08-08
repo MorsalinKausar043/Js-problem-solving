46a84a8d6c45f9c170652cbe4e59fe8fed454e0a
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-08 14:12:45 +0600

    add nested loop

---
+++ nested-loop.js
@@ -0,0 +1,18 @@
+
+// for (let i = 0; i <= 10; i++)
+// {
+//     for (let x = 0; x < i; x++)
+//     {
+//         console.log(x , " ");
+//     }
+// }
+
+
+
+for (let a = 1; a <= 5; a++)
+{
+    for (let b = 1; b <= a; b++)
+    {
+        console.log(b);
+    }
+}
