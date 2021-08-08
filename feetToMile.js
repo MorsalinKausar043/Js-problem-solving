b73a81cf61dd43f9413d49c0a278f3e587056388
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-08 14:31:16 +0600

    all done

---
+++ feetToMile.js
@@ -0,0 +1,19 @@
+// function feetToMile(feet) {
+    
+//     const Mile = feet / 5280;
+//     return Mile;
+// }
+
+// const feet = 6020;
+// const feetToMileFunc = feetToMile(feet);
+// console.log(feetToMileFunc);
+
+function feetHecto(feet) {
+    
+    const feetHectometer = feet / 328.084;
+    return feetHectometer;
+}
+
+const feet = 6020;
+const feetHectoFunc = feetHecto(feet);
+console.log(feetHectoFunc);
